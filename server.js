d98da2e9731605066ba45075e9f802cbd4256569
JaxxtotheMaxx <offbeat5024@gmail.com>
2020-04-02 16:06:24 -0400

    Update database connection information

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ var express = require('express');
 var app = express();                               //create our app with express
 var mongoose = require('mongoose');                //mongoose for mondodb
 var morgan = require('morgan');                    //log requests to the console (express4)
-var bodyParser = require('bodyParser');            //pull information from  HTML post (express4)
-var methodOverride = require('methodOverride');    //simulate DELETE and PUT (express4)
+var bodyParser = require('body-parser');            //pull information from  HTML post (express4)
+var methodOverride = require('method-override');    //simulate DELETE and PUT (express4)
 
 //Configuration =======================
 
-mongoose.connect('mongodb://node:user@mongo.onmodulus.net:27017/uw03mypu'); // connect to mongoDB database on modulus.io
+mongoose.connect('mongodb+srv://alex:<db-password>@mean-todo-bpbyr.mongodb.net/test?retryWrites=true&w=majority'
+); // connect to mongoDB database on modulus.io
 
 app.use(express.static(__dirname + '/public'));                   // set the static files location /public/img will be for users
 app.use(morgan('dev'));                                           // log ever request to the console
